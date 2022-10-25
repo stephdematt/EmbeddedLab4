7d4c09765d9a8f59835d5fc8f2174cdccba619b5
stephdematt <99276383+stephdematt@users.noreply.github.com>
2022-10-25 16:37:02 -0400

    Add files via upload

---
+++ Lab4_2/main.c
@@ -0,0 +1,18 @@
+#include <msp430.h>
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;                 // Stop WDT
+    P1DIR |= BIT6 | BIT7;                     // P1.6 and P1.7 output
+    P1SEL1 |= BIT6 | BIT7;                    // P1.6 and P1.7 options select
+    PM5CTL0 &= ~LOCKLPM5;
+    TB0CCR0 = 500000-1;                         // PWM Period
+    TB0CCTL1 = OUTMOD_7;                      // CCR1 reset/set
+    TB0CCR1 = 900;                            // CCR1 PWM duty cycle
+    TB0CCTL2 = OUTMOD_7;                      // CCR2 reset/set
+    TB0CCR2 = 100;                            // CCR2 PWM duty cycle
+    TB0CTL = TBSSEL__SMCLK | MC__UP | TBCLR;  // SMCLK, up mode, clear TBR
+    while(1)
+    {
+        P1OUT ^=BIT6;// For debugger
+    }
+}
