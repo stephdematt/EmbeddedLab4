501ff87bf5cc5d0d5db93d7c440e1629be839154
oliviaelwell <99269056+oliviaelwell@users.noreply.github.com>
2022-11-01 16:24:33 -0400

    Update main.c

--- Lab4_1/main.c
+++ Lab4_1/main.c
@@ -2,15 +2,15 @@
 int main(void)
 {
     WDTCTL = WDT_ADLY_250;                      // WDT 250ms, SMCLK, interval timer
-    P6OUT &= ~BIT6;                            //P.6 set up
-    P6DIR |= BIT6;
+    P6OUT &= ~BIT6;                            //P6.6 set up
+    P6DIR |= BIT6;                             //P
     PM5CTL0 &= ~LOCKLPM5;
-    SFRIE1 |= WDTIE;
-    _enable_interrupts();
+    SFRIE1 |= WDTIE;                          //
+    _enable_interrupts();                     //enables interruputs
     while (1);
     }
     #pragma vector=WDT_VECTOR
-    __interrupt void wdtled(void)
+    __interrupt void wdtled(void)            
     {
-            P6OUT^=BIT6;
+            P6OUT^=BIT6;                      //Makes the LED light up 
     }
