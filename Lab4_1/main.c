c3bf2b80b7158451daef3e43f799f9319d38ecc7
oliviaelwell <99269056+oliviaelwell@users.noreply.github.com>
2022-11-01 16:22:06 -0400

    Update main.c

--- Lab4_1/main.c
+++ Lab4_1/main.c
@@ -2,7 +2,7 @@
 int main(void)
 {
     WDTCTL = WDT_ADLY_250;                      // WDT 250ms, SMCLK, interval timer
-    P6OUT &= ~BIT6;
+    P6OUT &= ~BIT6;                            //P.6 set up
     P6DIR |= BIT6;
     PM5CTL0 &= ~LOCKLPM5;
     SFRIE1 |= WDTIE;
